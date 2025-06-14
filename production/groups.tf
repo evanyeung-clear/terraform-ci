e599eb1e56f8bfa3e297ca2ce8994485725dd15a
evanyeung-clear <81996685+evanyeung-clear@users.noreply.github.com>
2025-06-14 14:07:10 +0000

    .

--- production/groups.tf
+++ production/groups.tf
@@ -2,3 +2,8 @@ resource "okta_group" "example" {
   name        = "Example"
   description = "My Example Group"
 }
+
+resource "okta_group" "example-2" {
+  name        = "Example 2"
+  description = "My Example Group"
+}
