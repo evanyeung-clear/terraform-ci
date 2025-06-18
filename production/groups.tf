8cb2cfd12f09418db78e17c39c7b09ac8bb0d927
Evan Yeung <evan.yeung@clearme.com>
2025-06-17 20:58:01 -0400

    .

--- production/groups.tf
+++ production/groups.tf
@@ -2,3 +2,8 @@ resource "okta_group" "example" {
   name        = "Example"
   description = "My Example Group"
 }
+
+resource "okta_group" "example2" {
+  name        = "Example2"
+  description = "My Example Group"
+}
