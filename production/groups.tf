59f088ec1a29d58f6de9f33c1749fcea1cb0c198
evanyeung-clear <81996685+evanyeung-clear@users.noreply.github.com>
2025-07-26 23:36:22 -0400

    Update groups.tf

--- production/groups.tf
+++ production/groups.tf
@@ -7,3 +7,8 @@ resource "okta_group" "example2" {
   name        = "Example2"
   description = "My Example Group"
 }
+
+resource "okta_group" "example3" {
+  name        = "Example3"
+  description = "My Example Group"
+}
