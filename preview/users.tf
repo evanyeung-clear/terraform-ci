5b7094961535d001c99271f065cd9fb1c4b45666
evanyeung-clear <81996685+evanyeung-clear@users.noreply.github.com>
2025-12-10 11:41:29 -0500

    Update users.tf

--- preview/users.tf
+++ preview/users.tf
@@ -12,5 +12,10 @@ resource "okta_user" "test2" {
   email      = "example2@example.com"
 }
 
-
+resource "okta_user" "test3" {
+  first_name = "John"
+  last_name  = "Smith"
+  login      = "example3@example.com"
+  email      = "example3@example.com"
+}
 
