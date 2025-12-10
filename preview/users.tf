ae9844deea129696694d1c24e176640a11b36a8b
evanyeung-clear <81996685+evanyeung-clear@users.noreply.github.com>
2025-12-10 12:49:02 -0500

    Create users.tf (#48)
    * Create users.tf
    
    * .
    
    * .
    
    * Update users.tf
    
    * .
    
    * Update users.tf
    
    * Update users.tf
    
    * .
    
    * Update users.tf
    
    * .
    
    * Update users.tf
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * Update users.tf
    
    * .
    
    * .
    
    * Update users.tf
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * Create users.tf
    
    * Update users.tf
    
    * Update users.tf
    
    * Update users.tf
    
    * Update users.tf
    
    * Update users.tf
    
    * Update users.tf
    
    * Update users.tf
    
    * Update users.tf
    
    * Update users.tf
    
    * Update users.tf
    
    * Update users.tf
    
    * Update users.tf
    
    * Update users.tf
    
    * Update users.tf
    
    * Update users.tf
    
    * Update users.tf
    
    * Update users.tf
    
    * Update users.tf

---
+++ preview/users.tf
@@ -0,0 +1,29 @@
+resource "okta_user" "test1" {
+  first_name = "John"
+  last_name  = "Smith"
+  login      = "example@example.com"
+  email      = "example@example.com"
+}
+
+resource "okta_user" "test2" {
+  first_name = "John"
+  last_name  = "Smith"
+  login      = "example2@example.com"
+  email      = "example2@example.com"
+}
+
+resource "okta_user" "test3" {
+  first_name = "John"
+  last_name  = "Smith"
+  login      = "example3@example.com"
+  email      = "example3@example.com"
+}
+
+
+
+
+
+
+
+
+
