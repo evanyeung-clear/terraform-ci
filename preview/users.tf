3eba332fa63cf56bb9e5b3b35238948e2ca5b3c1
evanyeung-clear <81996685+evanyeung-clear@users.noreply.github.com>
2026-01-05 13:23:11 -0500

    Update users.tf

--- preview/users.tf
+++ preview/users.tf
@@ -19,17 +19,9 @@ resource "okta_user" "test3" {
   email      = "example3@example.com"
 }
 
-resource "okta_user" "test4" {
-  first_name = "John"
-  last_name  = "Smith"
-  login      = "example4@example.com"
-  email      = "example4@example.com"
-}
-
-
 resource "okta_user" "test" {
   for_each = tomap({
-    test4       = "example4@example.com"
+    test4 = "example4@example.com"
   })
 
   first_name = "John"
