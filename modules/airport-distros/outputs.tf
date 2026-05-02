1b83862d84e6576fea8a2e09a6c8cf464049e568
Evan Yeung <evan.yeung@clearme.com>
2026-05-02 04:38:08 -0400

    .

--- modules/airport-distros/outputs.tf
+++ modules/airport-distros/outputs.tf
@@ -1,11 +1,4 @@
 output "airport_id" {
   value = okta_group.airport.id
-}
-
-output "asst_managers_id" {
-  value = okta_group.asst_managers.id
-}
-
-output "managers_id" {
-  value = okta_group.managers.id
+  description = "Id of the airport group"
 }
